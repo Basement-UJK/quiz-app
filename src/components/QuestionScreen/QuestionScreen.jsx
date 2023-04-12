710edc7b36f7b75ab6212e710419b31f7a5673b7
bBravee <101563516+bBravee@users.noreply.github.com>
2023-04-12 21:29:20 +0200

    css changes

--- src/components/QuestionScreen/QuestionScreen.jsx
+++ src/components/QuestionScreen/QuestionScreen.jsx
@@ -5,24 +5,26 @@ import './QuestionScreen.css';
 
 function QuestionScreen() {
     return (
-        <Container className="question-container">
-            <div className="d-flex justify-content-center">
-                <div className="question">
-                    <p>Pytanie</p>
+        <div className="question-container">
+            <Container>
+                <div className="d-flex justify-content-center">
+                    <div className="question">
+                        <p>Pytanie</p>
+                    </div>
                 </div>
-            </div>
-            <Row className="info-row">
-                <Col>.NET</Col>
-                <Col>1/x</Col>
-                <Col>Lab x</Col>
-            </Row>
-            <hr />
-            <Row className="d-flex justify-content-center">
-                <Col className="answer" xl={12}>A</Col>
-                <Col className="answer" xl={12}>B</Col>
-                <Col className="answer" xl={12}>C</Col>
-            </Row>
-        </Container>
+                <Row className="info-row">
+                    <Col>.NET</Col>
+                    <Col>1/x</Col>
+                    <Col>Lab x</Col>
+                </Row>
+                <hr />
+                <Row className="d-flex justify-content-center">
+                    <Col className="mx-3 px-3 py-3 answer" xl={12}>A</Col>
+                    <Col className="mx-3 px-3 py-3 answer" xl={12}>B</Col>
+                    <Col className="mx-3 px-3 py-3 answer" xl={12}>C</Col>
+                </Row>
+            </Container>
+        </div>
     );
 }
 
