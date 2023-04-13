7ad10b67f42ac0f986c6b4a38aeaf61dca25857e
bBravee <101563516+bBravee@users.noreply.github.com>
2023-04-13 19:21:17 +0200

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,3 @@
-<<<<<<< HEAD
-import { useState } from 'react'
-
-import './App.css'
-
-import QuestionScreen from './components/QuestionScreen/QuestionScreen';
-import 'bootstrap/dist/css/bootstrap.min.css';
-
-function App() {
-  return (
-    <div className="App">
-      <QuestionScreen />
-=======
 import { useState } from "react";
 
 import "./App.css";
@@ -21,7 +8,6 @@ function App() {
   return (
     <div className="App">
       <MainPage />
->>>>>>> c513a9d2c78658b952b5e6c8f70328093b7cd1ea
     </div>
   );
 }
