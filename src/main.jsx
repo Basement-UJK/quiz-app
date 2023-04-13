9f9bc8728f8928a1f939377c154c05deae8b8522
Czechu7 <choocas0@gmail.com>
2023-04-13 19:38:47 +0200

    walepula

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import PythonPage from './PythonPage'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <PythonPage/>
   </React.StrictMode>,
 )
